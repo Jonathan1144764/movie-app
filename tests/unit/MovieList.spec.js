b8b5fdd98cddab940ccc9100d1dd816e53eced51
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-07 19:48:34 -0400

    Added tests to movie list

---
+++ tests/unit/MovieList.spec.js
@@ -0,0 +1,8 @@
+import { shallowMount } from "@vue/test-utils";
+
+describe("MovieList.vue", () => {
+  it("emits an event to add to favourites", () => {});
+  it("emits an event to remove from favourites", () => {});
+  it("emits an event to add to watchlist", () => {});
+  it("emits an event to remove from watchlist", () => {});
+});
