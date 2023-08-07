9d78ea04352f2e5375ebd7d64ba94619cc5a4e45
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-07 19:55:36 -0400

    Added tests for the user nav

---
+++ tests/unit/UserNav.spec.js
@@ -0,0 +1,9 @@
+import { shallowMount } from "@vue/test-utils";
+
+describe("UserNav.vue", () => {
+  it("changes the number of favourites movies when movies are favourited or unfavourited", () => {});
+  it("changes the number of movies in the watchlist when movies are added or removed from the watchlist", () => {});
+  it("emits an event to show the favourites list when the favourites button is clicked", () => {});
+  it("emits an event to show the watchlist when the watchlist button is clicked", () => {});
+  it("emits and event to display all movies when the view all button is clicked", () => {});
+});
