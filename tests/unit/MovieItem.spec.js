cf6c511d7d3295e64fbfaa9a9b07dba1dcd1ebaf
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-07 19:45:41 -0400

    Added tests for movie item

---
+++ tests/unit/MovieItem.spec.js
@@ -0,0 +1,18 @@
+import { shallowMount } from "@vue/test-utils";
+
+describe("MovieItem.vue", () => {
+  it("renders the props", () => {});
+  it("shows the button to add to favourites when the movie is not in favourites", () => {});
+  it("shows the button to add to watchlist when the movie is not in watchlist", () => {});
+  it("changes the text of the button to remove from favourites when the movie is in favourites", () => {});
+  it("changes the text of the button to remove from watchlist when the movie is in watchlist", () => {});
+  it("changes the class of the favourites button when the movie is in favourites", () => {});
+  it("changes the class of the favourites button when the movie is removed form favourites", () => {});
+  it("changes the class of the watchlist button when the movie is in watchlist", () => {});
+  it("changes the class of the watchlist button when the movie is removed form watchlist", () => {});
+  it("emits an event to add to favourites when the add to favourites button is clicked", () => {});
+  it("emits an event to remove from favourites when the remove from favourites button is clicked", () => {});
+  it("emits an event to add to watchlist when the add to watchlist button is clicked", () => {});
+  it("emits an event to remove from watchlist when the remove from watchlist button is clicked", () => {});
+  it("changes the class of the synopsis container to display when the view synopsis button is clicked", () => {});
+});
