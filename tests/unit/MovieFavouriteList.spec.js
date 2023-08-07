9be7efc5ddf4807303a3575138a1529abac384fe
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-07 19:38:10 -0400

    Added test file for favourite movie list

---
+++ tests/unit/MovieFavouriteList.spec.js
@@ -0,0 +1,7 @@
+import { shallowMount } from "@vue/test-utils";
+
+describe("MovieFavouriteList.vue", () => {
+  it("emits an action to remove from favourites", () => {});
+  it("emits an action to add to watchlist", () => {});
+  it("emits an action to remove from watchlist", () => {});
+});
