d720ad863aab72734ee733d89505cc76b6dc8e6f
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-08 15:52:21 -0400

    Deleted components and tests

--- tests/unit/MovieCreateReview.spec.js
+++
@@ -1,6 +0,0 @@
-import { shallowMount } from "@vue/test-utils";
-
-describe("MovieCreateReview.vue", () => {
-  it("displays the props", () => {});
-  it("adds the review when the post review button is clicked", () => {});
-});
--- tests/unit/MovieReviewList.spec.js
+++
@@ -1,6 +0,0 @@
-import { shallowMount } from "@vue/test-utils";
-
-describe("MovieReviewList.vue", () => {
-  it("shows the reviews if there are reviews associated with that movie", () => {});
-  it("emits an event to delete review", () => {});
-});
