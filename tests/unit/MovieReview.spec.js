a36674b264ee42479301d969c4d0a1adf6c1ccd2
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-08 10:16:31 -0400

    Added tests for movie review

---
+++ tests/unit/MovieReview.spec.js
@@ -0,0 +1,6 @@
+import { shallowMount } from "@vue/test-utils";
+
+describe("MovieReview.vue", () => {
+  it("displays the props", () => {});
+  it("emits an event to delete the review when the delete review button is clicked", () => {});
+});
