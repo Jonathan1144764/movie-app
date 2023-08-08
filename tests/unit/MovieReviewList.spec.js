f29a37579d88375126361608fd31f01466f58680
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-08 15:02:42 -0400

    Added test for movie review list

--- tests/unit/MovieReviewList.spec.js
+++ tests/unit/MovieReviewList.spec.js
@@ -1,5 +1,6 @@
 import { shallowMount } from "@vue/test-utils";
 
 describe("MovieReviewList.vue", () => {
+  it("shows the reviews if there are reviews associated with that movie", () => {});
   it("emits an event to delete review", () => {});
 });
