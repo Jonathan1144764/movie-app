118ac6e191ee5a44ca2c398694c3dfb8afc8912d
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-08 17:37:31 -0400

    Added tests for watchlist button

---
+++ tests/unit/WatchlistButton.spec.js
@@ -0,0 +1,10 @@
+import { shallowMount } from "@vue/test-utils";
+
+describe("Watchlist.vue", () => {
+  it("shows the text to add to watchlist when the movie is not in watchlist", () => {});
+  it("changes the class when the movie is in watchlist", () => {});
+  it("shows the text to remove from watchlist when the movie is in watchlist", () => {});
+  it("changes the class when the movie is not in watchlist", () => {});
+  it("adds the movie to watchlist when the button is clicked and the movie is not in watchlist", () => {});
+  it("removes the movie from watchlist when the button is clicked and the movie is in watchlist", () => {});
+});
