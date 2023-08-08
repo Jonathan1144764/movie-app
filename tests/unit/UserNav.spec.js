08c1a477546a5c3037ce7dc0308aa84b09bcb854
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-08 09:08:06 -0400

    Fixed typo

--- tests/unit/UserNav.spec.js
+++ tests/unit/UserNav.spec.js
@@ -5,5 +5,5 @@ describe("UserNav.vue", () => {
   it("changes the number of movies in the watchlist when movies are added or removed from the watchlist", () => {});
   it("emits an event to show the favourites list when the favourites button is clicked", () => {});
   it("emits an event to show the watchlist when the watchlist button is clicked", () => {});
-  it("emits and event to display all movies when the view all button is clicked", () => {});
+  it("emits an event to display all movies when the view all button is clicked", () => {});
 });
