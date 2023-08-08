18e9bd2565a1e8aeacd77407658172f7789f4c46
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-08 17:28:36 -0400

    Added tests for favourites button

---
+++ tests/unit/FavouritesButton.spec.js
@@ -0,0 +1,10 @@
+import { shallowMount } from "@vue/test-utils";
+
+describe("FavouritesButton.vue", () => {
+  it("shows the text to add to favourites when the movie is not in favourites", () => {});
+  it("changes the class when the movie is in favourites", () => {});
+  it("shows the text to remove from favourites when the movie is in favourites", () => {});
+  it("changes the class when the movie is not in favourites", () => {});
+  it("adds the movie to favourites when the button is clicked and the movie is not in favourites", () => {});
+  it("removes the movie from favourites when the button is clicked and the movie is in favourites", () => {});
+});
