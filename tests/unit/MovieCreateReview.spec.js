efc05783698aa159311eac2fdc0a560ee66e9cdb
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-08 10:33:53 -0400

    Added tests to create movie reviews

---
+++ tests/unit/MovieCreateReview.spec.js
@@ -0,0 +1,6 @@
+import { shallowMount } from "@vue/test-utils";
+
+describe("MovieCreateReview.vue", () => {
+  it("displays the props", () => {});
+  it("adds the review when the post review button is clicked", () => {});
+});
