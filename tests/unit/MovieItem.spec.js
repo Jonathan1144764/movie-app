6d4924471edf5ac5cd8aadb5d2738da3de360b2b
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-08 22:45:01 -0400

    Edited tests for movie item

--- tests/unit/MovieItem.spec.js
+++ tests/unit/MovieItem.spec.js
@@ -2,18 +2,10 @@ import { shallowMount } from "@vue/test-utils";
 
 describe("MovieItem.vue", () => {
   it("renders the props", () => {});
-  it("shows the button to add to favourites when the movie is not in favourites", () => {});
-  it("shows the button to add to watchlist when the movie is not in watchlist", () => {});
-  it("changes the text of the button to remove from favourites when the movie is in favourites", () => {});
-  it("changes the text of the button to remove from watchlist when the movie is in watchlist", () => {});
-  it("changes the class of the favourites button when the movie is in favourites", () => {});
-  it("changes the class of the favourites button when the movie is removed from favourites", () => {});
-  it("changes the class of the watchlist button when the movie is in watchlist", () => {});
-  it("changes the class of the watchlist button when the movie is removed from watchlist", () => {});
-  it("emits an event to add to favourites when the add to favourites button is clicked", () => {});
-  it("emits an event to remove from favourites when the remove from favourites button is clicked", () => {});
-  it("emits an event to add to watchlist when the add to watchlist button is clicked", () => {});
-  it("emits an event to remove from watchlist when the remove from watchlist button is clicked", () => {});
+  it("listens for an event to add the movie to favourites and emits an event in turn", () => {});
+  it("listens for an event to remove the movie from favourites and emits an event in turn", () => {});
+  it("listens for an event to add the movie to watchlist and emits an event in turn", () => {});
+  it("listens for an event to remove the movie from watchlist and emits an event in turn", () => {});
+  it("listens for an event to add movie rating and emits an event in turn", () => {});
   it("emits an event to display movie details when the see details button is clicked", () => {});
-  it("emits an event when the movie has been rated", () => {});
 });
