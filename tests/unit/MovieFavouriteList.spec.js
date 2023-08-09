face1056e0492bbee5eabb7f57acd46ff7b04abc
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-08 23:07:32 -0400

    Edited tests for favourite movie list

--- tests/unit/MovieFavouriteList.spec.js
+++ tests/unit/MovieFavouriteList.spec.js
@@ -1,7 +1,7 @@
 import { shallowMount } from "@vue/test-utils";
 
 describe("MovieFavouriteList.vue", () => {
-  it("emits an action to remove from favourites", () => {});
-  it("emits an action to add to watchlist", () => {});
-  it("emits an action to remove from watchlist", () => {});
+  it("listens for an event to remove from favourites and emits an event in turn", () => {});
+  it("listens for an event to add to watchlist and emits an event in turn", () => {});
+  it("listens for an event to remove from watchlist and emits an event in turn", () => {});
 });
