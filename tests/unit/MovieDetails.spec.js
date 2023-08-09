3db1f480aa9bc88781cd7aae539100500e2d15aa
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-08 22:47:16 -0400

    Edited tests for movie details

--- tests/unit/MovieDetails.spec.js
+++ tests/unit/MovieDetails.spec.js
@@ -1,5 +1,7 @@
 import { shallowMount } from "@vue/test-utils";
 
 describe("MovieDetails.vue", () => {
-  it("");
+  it("displays props", () => {});
+  it("listens for event to display movie details", () => {});
+  it("hides movie detials when the close button is clicked", () => {});
 });
