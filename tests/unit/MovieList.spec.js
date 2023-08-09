b54021b6f173ca8ffdf04493cc691efd5dddb85d
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-08 23:09:18 -0400

    Edited tests for movie watchlist

--- tests/unit/MovieList.spec.js
+++ tests/unit/MovieList.spec.js
@@ -1,8 +1,8 @@
 import { shallowMount } from "@vue/test-utils";
 
 describe("MovieList.vue", () => {
-  it("listens for an event when a movie is added to favourites", () => {});
-  it("listens for an event when a movie is removed from favourites", () => {});
-  it("listens for an event when a movie is added to watchlist", () => {});
-  it("listens for an event when a movie is removed from watchlist", () => {});
+  it("listens for an event when a movie is added to favourites and emits an event in turn", () => {});
+  it("listens for an event when a movie is removed from favourites and emits an event in turn", () => {});
+  it("listens for an event when a movie is added to watchlist and emits an event in turn", () => {});
+  it("listens for an event when a movie is removed from watchlist and emits an event in turn", () => {});
 });
--- tests/unit/MovieWatchlist.spec.js
+++ tests/unit/MovieWatchlist.spec.js
@@ -1,7 +1,7 @@
 import { shallowMount } from "@vue/test-utils";
 
 describe("MovieWatchlist.vue", () => {
-  it("emits an event to remove from watchlist", () => {});
-  it("emits an event to add to favourites", () => {});
-  it("emits an event to remove from favourites", () => {});
+  it("listens for an event to remove from watchlist and emits an event in turn", () => {});
+  it("listens for an event to add to favourites and emits an event in turn", () => {});
+  it("listens for an event to remove from favourites and emits an event in turn", () => {});
 });
