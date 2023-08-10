0780ada4111d4145fd4ec0147006c3557e4fc29b
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-10 16:27:01 -0400

    Added successful tests for movie list

--- tests/unit/MovieList.spec.js
+++ tests/unit/MovieList.spec.js
@@ -1,8 +1,25 @@
-// import { shallowMount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
+import MovieList from "@/components/MovieList.vue";
 
-// describe("MovieList.vue", () => {
-//   it("listens for an event when a movie is added to favourites and emits an event in turn", () => {});
-//   it("listens for an event when a movie is removed from favourites and emits an event in turn", () => {});
-//   it("listens for an event when a movie is added to watchlist and emits an event in turn", () => {});
-//   it("listens for an event when a movie is removed from watchlist and emits an event in turn", () => {});
-// });
+describe("MovieList.vue", () => {
+  it("listens for an event when a movie is added to favourites and emits an event in turn", async () => {
+    const wrapper = shallowMount(MovieList);
+    await wrapper.vm.addToFavourites(1);
+    expect(wrapper.emitted().addToFavourites[0][0]).toBe(1);
+  });
+  it("listens for an event when a movie is removed from favourites and emits an event in turn", async () => {
+    const wrapper = shallowMount(MovieList);
+    await wrapper.vm.removeFromFavourites(1);
+    expect(wrapper.emitted().removeFromFavourites[0][0]).toBe(1);
+  });
+  it("listens for an event when a movie is added to watchlist and emits an event in turn", async () => {
+    const wrapper = shallowMount(MovieList);
+    await wrapper.vm.addToWatchlist(1);
+    expect(wrapper.emitted().addToWatchlist[0][0]).toBe(1);
+  });
+  it("listens for an event when a movie is removed from watchlist and emits an event in turn", async () => {
+    const wrapper = shallowMount(MovieList);
+    await wrapper.vm.removeFromFavourites(1);
+    expect(wrapper.emitted().removeFromFavourites[0][0]).toBe(1);
+  });
+});
