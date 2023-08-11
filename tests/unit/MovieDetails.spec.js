542142749f81011c441521a316c0b84a1c8d4aeb
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-11 09:20:12 -0400

    Added successful test for movie details

--- tests/unit/MovieDetails.spec.js
+++ tests/unit/MovieDetails.spec.js
@@ -1,7 +1,24 @@
-// import { shallowMount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
+import MovieDetails from "@/components/MovieDetails.vue";
 
-// describe("MovieDetails.vue", () => {
-//   it("displays props", () => {});
-//   it("listens for an event to display movie details", () => {});
-//   it("hides movie detials when the close button is clicked", () => {});
-// });
+describe("MovieDetails.vue", () => {
+  it("displays props", () => {
+    const movie = {
+      id: 0,
+      title: "Default",
+      year: 1900,
+      director: "D Fault",
+      poster: "https://placehold.co/200x300/55ffcc/000",
+      isFavourited: false,
+      isToWatch: false,
+      rating: [],
+      synopsis: "A default movie synopsis.",
+    };
+    const wrapper = shallowMount(MovieDetails, {
+      props: { movie },
+    });
+    expect(wrapper.find("h3").text()).toBe("Default");
+  });
+  it("listens for an event to display movie details", () => {});
+  it("hides movie detials when the close button is clicked", () => {});
+});
