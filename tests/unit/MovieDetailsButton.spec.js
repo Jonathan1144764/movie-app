a1a68fde4da239bcc4ac9971c5c3db57fe02bd38
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-11 14:54:06 -0400

    removed tests for details button

--- tests/unit/MovieDetailsButton.spec.js
+++ tests/unit/MovieDetailsButton.spec.js
@@ -2,30 +2,9 @@ import { shallowMount } from "@vue/test-utils";
 import MovieDetailsButton from "@/components/MovieDetailsButton.vue";
 
 describe("MovieDetailsButtons", () => {
-  it("changes boolean state of isShown data to true when it is false", async () => {
-    const wrapper = shallowMount(MovieDetailsButton);
-    await wrapper.find("button").trigger("click");
-    expect(wrapper.vm.isShown).toBe(true);
-  });
-  it("changes boolean state of isShown to false when it is true", async () => {
-    const wrapper = shallowMount(MovieDetailsButton);
-    await wrapper.setData({
-      isShown: true,
-    });
-    await wrapper.find("button").trigger("click");
-    expect(wrapper.vm.isShown).toBe(false);
-  });
   it("emits event to show movie details component when clicked and isShown is false", async () => {
     const wrapper = shallowMount(MovieDetailsButton);
     await wrapper.find("button").trigger("click");
     expect(wrapper.emitted().showDetails[0][0]).toBe(0);
-  });
-  it("emits an event to hide movie details component when clicked and isShown is true", async () => {
-    const wrapper = shallowMount(MovieDetailsButton);
-    await wrapper.setData({
-      isShown: true,
-    });
-    await wrapper.find("button").trigger("click");
-    expect(wrapper.emitted().hideDetails[0][0]).toBe(0);
   });
 });
