56ebee5e4867f895430258f7c52ebc62f4dc2fd7
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-11 15:53:22 -0400

    created exit window component

---
+++ src/components/ExitWindowButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="hideWindow">X</button>
+</template>
+
+<script>
+export default {
+  name: "ExitWindowButton",
+  methods: {
+    hideWindow() {
+      this.$emit("hideWindow");
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
