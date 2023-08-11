1a8729b74af81aec237b29ab9e3f38b8ed180a1e
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-11 09:12:54 -0400

    Added movie details component

---
+++ src/components/MovieDetails.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="details-wrapper">
+    <MovieDetailsButton :movie="movie"></MovieDetailsButton>
+    <div class="details">
+      <h3>{{ movie.title }}</h3>
+      <p>{{ movie.year }} - {{ movie.director }}</p>
+      <p>{{ movie.synopsis }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import MovieDetailsButton from "./MovieDetailsButton.vue";
+
+export default {
+  name: "MovieDetails",
+  props: {
+    movie: {
+      type: Object,
+      default() {
+        return {
+          id: 0,
+          title: "Default",
+          year: 1900,
+          director: "D Fault",
+          poster: "https://placehold.co/200x300/55ffcc/000",
+          isFavourited: false,
+          isToWatch: false,
+          rating: [],
+          synopsis: "A default movie synopsis.",
+        };
+      },
+    },
+  },
+  components: {
+    MovieDetailsButton,
+  },
+};
+</script>
+
+<style scope lang="scss"></style>
