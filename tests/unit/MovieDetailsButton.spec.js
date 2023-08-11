177dca80bf9e57b519d85b15c467a6b4c1d66908
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-11 15:26:27 -0400

    update movie details button test desc

--- tests/unit/MovieDetailsButton.spec.js
+++ tests/unit/MovieDetailsButton.spec.js
@@ -2,7 +2,7 @@ import { shallowMount } from "@vue/test-utils";
 import MovieDetailsButton from "@/components/MovieDetailsButton.vue";
 
 describe("MovieDetailsButtons", () => {
-  it("emits event to show movie details component when clicked and isShown is false", async () => {
+  it("emits event to show movie details component when clicked", async () => {
     const wrapper = shallowMount(MovieDetailsButton);
     await wrapper.find("button").trigger("click");
     expect(wrapper.emitted().showDetails[0][0]).toBe(0);
