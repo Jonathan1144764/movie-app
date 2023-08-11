275623ded928d2c6476d5d6920ac1c371be6ae9e
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-10 22:30:13 -0400

    Created movie details button and test file

---
+++ src/components/MovieDetailsButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <button v-if="this.isShown == false">See details</button>
+  <button v-else>X</button>
+</template>
+
+<script>
+export default {
+  name: "MovieDetailsButton",
+  props: {
+    movie: {
+      type: Object,
+      default() {
+        return {
+          id: 0,
+          title: "Default",
+          year: 1900,
+          director: "D Fault",
+          poster: "https://placehold.co/200x300/55ffcc/000",
+          isFavourited: false,
+          isToWatch: false,
+          rating: [],
+          synopsis: "A default movie synopsis.",
+        };
+      },
+    },
+  },
+  data() {
+    return {
+      isShown: false,
+    };
+  },
+  methods: {
+    showDetails() {
+      this.$emit("showDetails", this.movie.id);
+      console.log(this.movie.id);
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
