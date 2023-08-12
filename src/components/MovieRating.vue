39f8e56371a9b80027541091f0ab1788d8c0f910
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-12 17:16:29 -0400

    Created rating component

---
+++ src/components/MovieRating.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "MovieRating",
+    methods: {
+        addRating() {
+            this.$emit("addRating")
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss"></style>
