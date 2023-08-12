7d07d14520315e8e25f4e4206f4f864df5f83b9f
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-12 16:52:37 -0400

    Added tests for watchlist

--- tests/unit/MovieWatchlist.spec.js
+++ tests/unit/MovieWatchlist.spec.js
@@ -1,7 +1,20 @@
-// import { shallowMount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
+import MovieWatchlist from "@/components/MovieWatchlist.vue";
 
-// describe("MovieWatchlist.vue", () => {
-//   it("listens for an event to remove from watchlist and emits an event in turn", () => {});
-//   it("listens for an event to add to favourites and emits an event in turn", () => {});
-//   it("listens for an event to remove from favourites and emits an event in turn", () => {});
-// });
+describe("MovieWatchlist.vue", () => {
+  it("listens for an event to remove from watchlist and emits an event in turn", async () => {
+    const wrapper = shallowMount(MovieWatchlist);
+    await wrapper.vm.removeFromWatchlist(1);
+    expect(wrapper.emitted().removeFromWatchlist[0][0]).toBe(1);
+  });
+  it("listens for an event to add to favourites and emits an event in turn", async () => {
+    const wrapper = shallowMount(MovieWatchlist);
+    await wrapper.vm.addToFavourites(1);
+    expect(wrapper.emitted().addToFavourites[0][0]).toBe(1);
+  });
+  it("listens for an event to remove from favourites and emits an event in turn", async () => {
+    const wrapper = shallowMount(MovieWatchlist);
+    await wrapper.vm.removeFromFavourites(1);
+    expect(wrapper.emitted().removeFromFavourites[0][0]).toBe(1);
+  });
+});
