072aa147f8f91db430af5f51833c4e3c60fb681f
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-12 20:30:06 -0400

    Edited grammar

--- tests/unit/MovieRating.spec.js
+++ tests/unit/MovieRating.spec.js
@@ -2,7 +2,7 @@ import { shallowMount } from "@vue/test-utils";
 import MovieRating from "@/components/MovieRating.vue";
 
 describe("MovieRating.vue", () => {
-  it("emits an event to add a rating to the movie's average rating when the stars are clicked", async () => {
+  it("emits an event to add a rating to the movie's average rating when any star is clicked", async () => {
     const wrapper = shallowMount(MovieRating);
     await wrapper.find("#rs1").trigger("click");
     expect(wrapper.emitted().addRating[0][1]).toBe(1);
