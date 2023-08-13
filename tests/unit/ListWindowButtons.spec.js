b6314c8f5159a613b26c9548593ae3ffcb49d8e6
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-13 15:32:15 -0400

    rename file

--- tests/unit/ListWindowButtons.spec.js
+++ tests/unit/ListWindowButtons.spec.js
@@ -1,8 +1,11 @@
 import { shallowMount } from "@vue/test-utils";
-import ListWindowButtons from "@/components/ListWindoeButtons.vue";
+import ListWindowButtons from "@/components/ListWindowButtons.vue";
 
 describe("ListWindowButtons", () => {
-  it("displays favourited movies prop", () => {});
+  it("displays favourited movies prop", () => {
+    const wrapper = shallowMount(ListWindowButtons);
+    console.log(wrapper);
+  });
   it("displays movies to watch prop", () => {});
   it("emits an event to show fvaourites list", () => {});
   it("emits an event to show watchlist", () => {});
