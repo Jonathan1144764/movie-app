ee0ca70089684c16b206899b32e63c9b8eeed7ec
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-13 15:29:16 -0400

    Added file to test list window buttons

---
+++ tests/unit/ListWondowButtons.spec.js
@@ -0,0 +1,9 @@
+import { shallowMount } from "@vue/test-utils";
+import ListWindowButtons from "@/components/ListWindoeButtons.vue";
+
+describe("ListWindowButtons", () => {
+  it("displays favourited movies prop", () => {});
+  it("displays movies to watch prop", () => {});
+  it("emits an event to show fvaourites list", () => {});
+  it("emits an event to show watchlist", () => {});
+});
