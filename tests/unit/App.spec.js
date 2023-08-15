86fad4d35c0ca2514d34945c00140fc1bfc5c041
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-14 21:54:10 -0400

    Added final tests

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -65,4 +65,110 @@ describe("App", () => {
     expect(wrapper.vm.moviesToWatch).toBe(0);
     expect(wrapper.vm.moviestoWatchList).toBeFalsy();
   });
+  it("listens for an event to add movie to favourites and change movie isFavourited data to true", async () => {
+    const wrapper = shallowMount(App);
+    await wrapper.setData({
+      movies: [
+        {
+          id: 1,
+          title: "Monty Python and the Holy Grail",
+          year: 1975,
+          director: "Terry Gilliam",
+          poster: "https://flxt.tmsimg.com/assets/p629_p_v8_af.jpg",
+          isFavourited: false,
+          isToWatch: false,
+          rating: [],
+          synopsis:
+            "King Arthur and his Knights of the Round Table embark on a surreal, low-budget search for the Holy Grail, encountering many, very silly obstacles.",
+        },
+      ],
+      moviesFavourited: 0,
+      moviesFavouritedList: [],
+    });
+    await wrapper.vm.addToFavourites(1);
+    expect(wrapper.vm.movies[0].isFavourited).toBe(true);
+    expect(wrapper.vm.moviesFavourited).toBe(1);
+    expect(wrapper.vm.moviesFavouritedList[0].id).toBe(1);
+  });
+  it("listens for an event to remove movie from favourites and change movie isFavourited data to false", async () => {
+    const wrapper = shallowMount(App);
+    await wrapper.setData({
+      movies: [
+        {
+          id: 1,
+          title: "Monty Python and the Holy Grail",
+          year: 1975,
+          director: "Terry Gilliam",
+          poster: "https://flxt.tmsimg.com/assets/p629_p_v8_af.jpg",
+          isFavourited: true,
+          isToWatch: false,
+          rating: [],
+          synopsis:
+            "King Arthur and his Knights of the Round Table embark on a surreal, low-budget search for the Holy Grail, encountering many, very silly obstacles.",
+        },
+      ],
+      moviesFavourited: 1,
+      moviesFavouritedList: [
+        {
+          id: 1,
+          title: "Monty Python and the Holy Grail",
+          year: 1975,
+          director: "Terry Gilliam",
+          poster: "https://flxt.tmsimg.com/assets/p629_p_v8_af.jpg",
+          isFavourited: true,
+          isToWatch: false,
+          rating: [],
+          synopsis:
+            "King Arthur and his Knights of the Round Table embark on a surreal, low-budget search for the Holy Grail, encountering many, very silly obstacles.",
+        },
+      ],
+    });
+    await wrapper.vm.removeFromFavourites(1);
+    expect(wrapper.vm.movies[0].isFavourited).toBe(false);
+    expect(wrapper.vm.moviesFavourited).toBe(0);
+    expect(wrapper.vm.moviesFavouritedList).toStrictEqual([]);
+  });
+  it("listens for an event to add rating to movies data rating", async () => {
+    const wrapper = shallowMount(App);
+    await wrapper.setData({
+      movies: [
+        {
+          id: 1,
+          title: "Monty Python and the Holy Grail",
+          year: 1975,
+          director: "Terry Gilliam",
+          poster: "https://flxt.tmsimg.com/assets/p629_p_v8_af.jpg",
+          isFavourited: false,
+          isToWatch: false,
+          rating: [],
+          synopsis:
+            "King Arthur and his Knights of the Round Table embark on a surreal, low-budget search for the Holy Grail, encountering many, very silly obstacles.",
+        },
+      ],
+    });
+    await wrapper.vm.addRating(1, 2);
+    expect(wrapper.vm.movies[0].rating[0]).toBe(2);
+  });
+  it("listens for an event that changes the favouritesShown data to true when false", async () => {
+    const wrapper = shallowMount(App);
+    await wrapper.vm.showFavourites();
+    expect(wrapper.vm.favouritesShown).toBe(true);
+  });
+  it("listens for an event that changes the favouritesShown data to false when true", async () => {
+    const wrapper = shallowMount(App);
+    await wrapper.setData({ favouritesShown: true });
+    await wrapper.vm.hideFavouritesWindow();
+    expect(wrapper.vm.favouritesShown).toBe(false);
+  });
+  it("listens for an event that changes the toWatchShown data to true when false", async () => {
+    const wrapper = shallowMount(App);
+    await wrapper.vm.showToWatch();
+    expect(wrapper.vm.toWatchShown).toBe(true);
+  });
+  it("listens for an event that changes the toWatchShown data to false when true", async () => {
+    const wrapper = shallowMount(App);
+    await wrapper.setData({ toWatchShown: true });
+    await wrapper.vm.hideToWatchWindow();
+    expect(wrapper.vm.toWatchShown).toBe(false);
+  });
 });
