4437c1ff11256a9df92756e7d086fb85a25d6fc7
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-16 15:24:18 -0400

    Updated test specs

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -23,6 +23,7 @@ describe("App", () => {
       moviesToWatchList: [],
     });
     await wrapper.vm.addToWatchlist(1);
+    expect(wrapper.vm.addToWatchlist).toBeTruthy();
     expect(wrapper.vm.movies[0].isToWatch).toBe(true);
     expect(wrapper.vm.moviesToWatch).toBe(1);
     expect(wrapper.vm.moviesToWatchList[0].id).toBe(1);
@@ -61,6 +62,7 @@ describe("App", () => {
       ],
     });
     await wrapper.vm.removeFromWatchlist(1);
+    expect(wrapper.vm.removeFromWatchlist).toBeTruthy();
     expect(wrapper.vm.movies[0].isToWatch).toBe(false);
     expect(wrapper.vm.moviesToWatch).toBe(0);
     expect(wrapper.vm.moviestoWatchList).toBeFalsy();
@@ -86,6 +88,7 @@ describe("App", () => {
       moviesFavouritedList: [],
     });
     await wrapper.vm.addToFavourites(1);
+    expect(wrapper.vm.addToFavourites).toBeTruthy();
     expect(wrapper.vm.movies[0].isFavourited).toBe(true);
     expect(wrapper.vm.moviesFavourited).toBe(1);
     expect(wrapper.vm.moviesFavouritedList[0].id).toBe(1);
@@ -124,6 +127,7 @@ describe("App", () => {
       ],
     });
     await wrapper.vm.removeFromFavourites(1);
+    expect(wrapper.vm.removeFromFavourites).toBeTruthy();
     expect(wrapper.vm.movies[0].isFavourited).toBe(false);
     expect(wrapper.vm.moviesFavourited).toBe(0);
     expect(wrapper.vm.moviesFavouritedList).toStrictEqual([]);
@@ -147,28 +151,33 @@ describe("App", () => {
       ],
     });
     await wrapper.vm.addRating(1, 2);
+    expect(wrapper.vm.addRating).toBeTruthy();
     expect(wrapper.vm.movies[0].rating[0]).toBe(2);
   });
   it("listens for an event that changes the favouritesShown data to true when false", async () => {
     const wrapper = shallowMount(App);
     await wrapper.vm.showFavourites();
+    expect(wrapper.vm.showFavourites).toBeTruthy();
     expect(wrapper.vm.favouritesShown).toBe(true);
   });
   it("listens for an event that changes the favouritesShown data to false when true", async () => {
     const wrapper = shallowMount(App);
     await wrapper.setData({ favouritesShown: true });
     await wrapper.vm.hideFavouritesWindow();
+    expect(wrapper.vm.hideFavouritesWindow).toBeTruthy();
     expect(wrapper.vm.favouritesShown).toBe(false);
   });
   it("listens for an event that changes the toWatchShown data to true when false", async () => {
     const wrapper = shallowMount(App);
     await wrapper.vm.showToWatch();
+    expect(wrapper.vm.showToWatch).toBeTruthy();
     expect(wrapper.vm.toWatchShown).toBe(true);
   });
   it("listens for an event that changes the toWatchShown data to false when true", async () => {
     const wrapper = shallowMount(App);
     await wrapper.setData({ toWatchShown: true });
     await wrapper.vm.hideToWatchWindow();
+    expect(wrapper.vm.hideToWatchWindow).toBeTruthy();
     expect(wrapper.vm.toWatchShown).toBe(false);
   });
 });
