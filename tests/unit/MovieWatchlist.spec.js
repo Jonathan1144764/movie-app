362c5df44f30ae5bedbf7e94a6b6c46ac55394d8
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-08-16 15:50:37 -0400

    Updated test specs

--- tests/unit/MovieWatchlist.spec.js
+++ tests/unit/MovieWatchlist.spec.js
@@ -5,26 +5,31 @@ describe("MovieWatchlist.vue", () => {
   it("listens for an event to remove from watchlist and emits an event in turn", async () => {
     const wrapper = shallowMount(MovieWatchlist);
     await wrapper.vm.removeFromWatchlist(1);
+    expect(wrapper.vm.removeFromWatchlist).toBeTruthy();
     expect(wrapper.emitted().removeFromWatchlist[0][0]).toBe(1);
   });
   it("listens for an event to add to favourites and emits an event in turn", async () => {
     const wrapper = shallowMount(MovieWatchlist);
     await wrapper.vm.addToFavourites(1);
+    expect(wrapper.vm.addToFavourites).toBeTruthy();
     expect(wrapper.emitted().addToFavourites[0][0]).toBe(1);
   });
   it("listens for an event to remove from favourites and emits an event in turn", async () => {
     const wrapper = shallowMount(MovieWatchlist);
     await wrapper.vm.removeFromFavourites(1);
+    expect(wrapper.vm.removeFromFavourites).toBeTruthy();
     expect(wrapper.emitted().removeFromFavourites[0][0]).toBe(1);
   });
   it("listens for an event to add movie rating and emits an event in turn", async () => {
     const wrapper = shallowMount(MovieWatchlist);
     await wrapper.vm.addRating(1, 2);
+    expect(wrapper.vm.addRating).toBeTruthy();
     expect(wrapper.emitted().addRating[0][1]).toBe(2);
   });
   it("listens for an event to hide the section window and emits an event in turn", async () => {
     const wrapper = shallowMount(MovieWatchlist);
     await wrapper.vm.hideWindow();
+    expect(wrapper.vm.hideWindow).toBeTruthy();
     expect(wrapper.emitted().hideWindow[0][0]).toBe(true);
   });
   it("changes the class of the section whe toWatchShown is true", async () => {
